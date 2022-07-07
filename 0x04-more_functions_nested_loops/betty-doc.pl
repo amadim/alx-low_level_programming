cc7d6a7a59cacec2d1e3a0a6bd2543a66e02e629
amadim <amadimariana@gmail.com>
2022-07-07 07:41:48 -0700

    betty-doc.pl

---
+++ 0x04-more_functions_nested_loops/betty-doc.pl
@@ -0,0 +1,8 @@
+## This software falls under the GNU General Public License.     ##
+## Please read the COPYING file for more information             ##
+
+# 18/01/2001 - 	Cleanups
+#                         (...)* signifies 0 or more structure elements
+# /**
+#  * function_name(:)? (- short description)?
+# (* @parameterx: (description of parameter x)?)*
