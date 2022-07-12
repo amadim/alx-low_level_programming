e4af006a64464ff9d962060f5d114430e70bee9f
amadim <amadimariana@gmail.com>
2022-07-12 08:26:30 -0700

    pointer task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strlen - Return the length of a given string
+ * @s: Pointer to the string
+ *
+ * Return: 0
+ */
+int _strlen(char *s)
+{
+	int i;
+
+	while (*(s + i) != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
