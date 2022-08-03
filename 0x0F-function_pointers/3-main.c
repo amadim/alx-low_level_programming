b777d40b3febd0259cea36bdc87a11d4eaf4c853
amadim <amadimariana@gmail.com>
2022-08-03 04:44:11 -0700

    3-main.c

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,33 +1,33 @@
- (27 sloc)  401 Bytes
-#include "3-calc.h"
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include "3-calc.h"
 
 /**
-  * main - ...
-  * @argc: ...
-  * @argv: ...
-  *
-  * Return: ...
-  */
+ * main - performs simple operations
+ * @argc: number of arguments passed
+ * @argv: array of pointers to arguments
+ *
+ * Return: always 0
+ */
 int main(int argc, char *argv[])
 {
-	int (*oprt)(int, int);
+	int a, b, c;
+	int (*f)(int, int);
 
 	if (argc != 4)
 	{
 		printf("Error\n");
 		exit(98);
 	}
-
-	oprt = get_op_func(argv[2]);
-
-	if (!oprt)
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
+	f = get_op_func(argv[2]);
+	if (f == NULL)
 	{
 		printf("Error\n");
 		exit(99);
 	}
-
-	printf("%d\n", oprt(atoi(argv[1]), atoi(argv[3])));
+	c = f(a, b);
+	printf("%d\n", c);
 	return (0);
 }
